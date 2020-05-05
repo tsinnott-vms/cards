20ed4852d2fb49e4143096a5b52792d94c2e6577
jverratti <31357738+jverratti@users.noreply.github.com>
2020-05-05 13:20:19 -0600

    created card class

--- src/App.java
+++ src/App.java
@@ -1,24 +1,24 @@
 import processing.core.PApplet;
 
-public class App extends PApplet
-{
-    public static void main(String[] args)
-    {
+public class App extends PApplet {
+
+    Card c = new Card();
+
+    public static void main(String[] args) {
         PApplet.main("App");
     }
 
-}
 
-    public void settings()
-    {
-        size(800,600);
-    }
+    public void settings() {
+        size(800, 600);
 
-    Rank r = new Rank ("Jack", 11);
-    Rank jack = new new Rank("Jack", 11);
-    Rank ace = new Rank ("Ace", 14);
 
-        if (ace.getValue() > jack.getValue())
-        {
-        //ace is greater
+        Rank r = new Rank("Jack", 11);
+        Rank jack = new Rank("Jack", 11);
+        Rank ace = new Rank("Ace", 14);
+
+        if (ace.getValue() > jack.getValue()) {
+            //ace is greater
         }
+    }
+}
